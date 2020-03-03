ef249d132a6808031e8b66e8fc8d4feb3c2ebb0f
Luram Archanjo <luram.archanjo@sensedia.com>
2020-03-02 21:15:55 -0300

    Initial Spring apache avro integration

---
+++ apache-avro/src/main/kotlin/com/example/apacheavro/ApacheAvroApplication.kt
@@ -0,0 +1,11 @@
+package com.example.apacheavro
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApacheAvroApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApacheAvroApplication>(*args)
+}
