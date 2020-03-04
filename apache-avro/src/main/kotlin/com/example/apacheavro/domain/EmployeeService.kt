83fb775230c024b880604f236d4cfea55b36842f
Luram Archanjo <luram.archanjo@sensedia.com>
2020-03-03 21:55:18 -0300

    Apache Avro

--- apache-avro/src/main/kotlin/com/example/apacheavro/domain/EmployeeService.kt
+++ apache-avro/src/main/kotlin/com/example/apacheavro/domain/EmployeeService.kt
@@ -1,21 +1,34 @@
 package com.example.apacheavro.domain
 
+import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.slf4j.LoggerFactory
-import org.springframework.cloud.stream.schema.SchemaReference
-import org.springframework.cloud.stream.schema.client.SchemaRegistryClient
+import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
-class EmployeeService(val schemaRegistryClient: SchemaRegistryClient) {
+class EmployeeService(val kafkaTemplate: KafkaTemplate<String, Employee>) {
 
     private final val log = LoggerFactory.getLogger(this::class.java)
 
-    @Scheduled(initialDelay = 1000, fixedDelay = 60000)
-    fun fetchSchema() {
-        val schemaReference = SchemaReference("customer-schema", 1, "application/json")
-        val schema = schemaRegistryClient.fetch(schemaReference)
-        log.info(schema)
+    @Scheduled(initialDelay = 1000, fixedDelay = 3000)
+    fun producer() {
+        log.info("Generating random data")
+        val employee = Employee()
+        employee.setId(Random().nextInt())
+        employee.setFirstName(UUID.randomUUID().toString())
+        employee.setLastName(UUID.randomUUID().toString())
+
+        kafkaTemplate.sendDefault(employee)
+        log.info("Generated random data")
+    }
+
+    @KafkaListener(topics = ["\${spring.kafka.template.default-topic}"])
+    fun consumer(record: ConsumerRecord<String, Employee>) {
+        log.info("Consuming $record")
+        log.info("Consumed $record")
     }
 
 }
